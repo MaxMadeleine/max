24bfb265c3bf035507cad8da60f17ecef129720b
Max Madeleine <max.madeleine.g@gmail.com>
2024-12-23 17:01:38 +0100

    sd

---
+++ index.php
@@ -0,0 +1,12 @@
+<style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+<?php
+$emy = "emanuel ";
+$age = $_GET["age"] + 34;
+
+echo $emy . "is gay and " . $age . " years old";
+?>
