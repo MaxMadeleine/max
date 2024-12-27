6adcc00b5306b23e4dddcb5c7cb52620633817d1
Max Madeleine <max.madeleine.g@gmail.com>
2024-12-27 12:45:58 +0100

    get complete

--- i.php
+++ i.php
@@ -1,3 +1,22 @@
 <?php
-echo "helo";
-phpinfo();
+
+$action = $_GET["action"];
+$w1 = $_GET["w1"];
+$w2 = $_GET["w2"];
+$variable; 
+
+if ($action == "before") {
+
+    $variable = $w1 . " - " . $w2;
+
+} elseif ($action == "after") {
+
+    $variable = $w2 . " - " . $w2;
+
+} else {
+
+    $variable = "DUMMY";
+
+}
+?>
+<h1><?php echo $variable ?></h1>
--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
-if($_GET["n1"] + $_GET["n2"] == "420")
-{echo "smoke weed"; }
-else{
-echo "wow let emanuel tell you what you did wrong =D";
+
+$n1 = $_GET["n1"];
+$n2 = $_GET["n2"];
+$action = $_GET["action"];
+$echo;
+
+if( empty($n1) || empty($n2)){
+  return;
 }
+
+if( $action == "plus" ){
+  $echo = $n1 . " + " . $n2 . " = " . $n1 + $n2;
+}
+elseif( $action == "minus" ){
+  $echo = $n1 - $n2;
+}
+elseif( $action == "div" ){
+  $echo = $n1 / $n2;
+}
+elseif( $action == "add" ){
+  $echo = $n1 * $n2;
+}
+else {echo "error";}
+?>
+
+<html>
+    <h1> <?php echo $echo;  ?></h1>
+</html>
